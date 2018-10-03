628b12416e1375760856587f752fde3bac1ae2af
Donna Cadiente <djanecad@hotmail.com>
2018-10-03 20:06:22 +0000

    automatically backed up by learn

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ document.addEventListener("DOMContentLoaded", e => {
   document.querySelector("body")
    .addEventListener("click", e => console.log("Reggae, Reggae!"));
 })
+
+document.addEventListener("DOMContentLoaded", e => {document.querySelector})
